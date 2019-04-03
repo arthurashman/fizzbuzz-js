52918de590a531d9ae3881280d35495261aca7a2
Arthur Ashman <arthurashman@gmail.com>
2019-04-03 16:08:14 +0100

    FizzBuzz in javascript complete

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -2,11 +2,48 @@ describe('FizzBuzz', function() {
 
   var fizzBuzz;
 
+  beforeEach(function() {
+    fizzBuzz = new FizzBuzz();
+  });
+
   describe('knows when a number is', function() {
     it('divisible by 3', function() {
-      fizzBuzz = new FizzBuzz();
-      expect(isDivisibleByThree(3)).toBe(true);
+      expect(fizzBuzz.isDivisibleByThree(3)).toBe(true);
     });
-   
+    it('divisible by 5', function() {
+      expect(fizzBuzz.isDivisibleByFive(5)).toBe(true);
+    });
+    it('divisble by 15', function() {
+      expect(fizzBuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
   });
+
+  describe('knows when a number is NOT', function() {
+    it('divisible by 3', function() {
+      expect(fizzBuzz.isDivisibleByThree(2)).toBe(false);
+    });
+    it('divisible by 5', function() {
+      expect(fizzBuzz.isDivisibleByFive(7)).toBe(false);
+    });
+    it('divisible by 15', function() {
+      expect(fizzBuzz.isDivisibleByFifteen(34)).toBe(false);
+    });
+  });
+  
+  describe('when playing, says', function() {
+    it('Fizz when a number is divisible by 3', function() {
+      expect(fizzBuzz.says(3)).toEqual("Fizz");
+    });
+    it('Buzz when a number is divisble by 5', function() {
+      expect(fizzBuzz.says(5)).toEqual("Buzz");
+    });
+    it('FizzBuzz when a number is divisible by 3 and 5', function() {
+      expect(fizzBuzz.says(30)).toEqual("FizzBuzz");
+    });
+    it('number when number is not a FizzBuzz number', function(){
+      expect(fizzBuzz.says(7)).toEqual(7);
+    });
+    
+  });
+
 });
